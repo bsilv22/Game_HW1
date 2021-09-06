a96dc13b1390ea00295378baae8cb7f5d736af72
bsilv22 <bsilv22@yahoo.com>
2021-09-06 19:07:27 -0400

    Finished some Constructors

---
+++ Game_HW1/Game.cpp
@@ -0,0 +1,4 @@
+#include "Game.h"
+
+//This is the default constructor definition
+game::game() { title = "Game Title"; esrb = "Everyone"; price = 1; }
--- Game_HW1/Game.h
+++ Game_HW1/Game.h
@@ -1,3 +1,25 @@
+//****************************************************
+// File: Game.h
+//
+// Purpose: Class made to store game information
+//
+// Written By: Brett Silver
+//
+// Compiler: Visual C++ 2019
+//
+// Update Information
+// ------------------
+//
+// Name: Brett Silver
+// Date: 9/6/2021
+// Description: Includes member variables, constructors, and getters and setters
+//
+// Name: Brett Silver
+// Date: 9/6/2021
+//@Brett Silver
+//
+//**************************************************** 
+
 #pragma once
 #include <string>
 using namespace std;
@@ -9,6 +31,12 @@ private:
 
 	string title;
 	string esrb;
+	double price;
+
+public:
+	game();
+	//Parameterized Constructor 
+	game(string theTitle, string theEsrb, double thePrice) { title = theTitle; esrb = theEsrb; price = thePrice; }
 
 
 };
